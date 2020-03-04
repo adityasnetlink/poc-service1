42611cdffc76d15263c53bc0298cd2fa7ffae789
Aditya Sharma <adityasharma@Ayushs-Mac-mini.local>
2020-03-04 13:30:45 +0530

    new

--- src/main/java/com/example/demo/SchedulerComponent.java
+++ src/main/java/com/example/demo/SchedulerComponent.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
--- src/main/java/com/example/demo/WelcomeConfiguration.java
+++ src/main/java/com/example/demo/WelcomeConfiguration.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "welcome")
